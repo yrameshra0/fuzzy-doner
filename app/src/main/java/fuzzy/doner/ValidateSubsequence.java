e9dfbe52fe7f3f42f28616cf5f61fcddd5fdfe44
Y Ramesh Rao <yrameshrao@gmail.com>
2021-11-09 19:13:29 +0530

    add: validate of subsequence init

---
+++ app/src/main/java/fuzzy/doner/ValidateSubsequence.java
@@ -0,0 +1,9 @@
+package fuzzy.doner;
+
+import java.util.List;
+
+public class ValidateSubsequence {
+    public static boolean isValidSubsequence(List<Integer> inputArray, List<Integer> sequence) {
+        return true;
+    }
+}
---
+++ app/src/test/java/fuzzy/doner/ValidateSubsequenceTest.java
@@ -0,0 +1,28 @@
+package fuzzy.doner;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ValidateSubsequenceTest {
+
+    @Test
+    public void nonContiguousSubsequence() {
+        List<Integer> inputArray = Arrays.asList(5, 1, 22, 25, 6, -1, 8, 10);
+        List<Integer> sequence = Arrays.asList(1, 6, -1, 10);
+
+        assertThat(ValidateSubsequence.isValidSubsequence(inputArray, sequence), is(true));
+    }
+
+    @Test
+    public void contiguousSubsequence() {
+        List<Integer> inputArray = Arrays.asList(5, 1, 22, 25, 6, -1, 8, 10);
+        List<Integer> sequence = Arrays.asList(1, 22, 25, 6);
+
+        assertThat(ValidateSubsequence.isValidSubsequence(inputArray, sequence), is(true));
+    }
+}

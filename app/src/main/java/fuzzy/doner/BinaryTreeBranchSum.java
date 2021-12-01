bb33c654b14a5f191f71c8d78bbf0841c68dbe21
Y Ramesh Rao <yrameshrao@gmail.com>
2021-12-01 22:43:43 +0530

    draft: Binary Tree Sum

--- app/src/main/java/fuzzy/doner/BinaryTreeBranchSum.java
+++ app/src/main/java/fuzzy/doner/BinaryTreeBranchSum.java
@@ -1,10 +1,31 @@
 package fuzzy.doner;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BinaryTreeBranchSum {
-    public static List<Integer> generate() {
-        return null;
+    private static Map<String, Integer> memoSum = new HashMap<>();
+
+    public static List<Integer> generate(BinaryTree root) {
+        memoSum.put(root.value+"", root.value);
+        sumOfBranches(root, root.value+"");
+        return memoSum.keySet().stream()
+                .map(value->value.contains("LEAFSUM"))
+                .map(value->memoSum.get(value))
+                .collect(Collectors.toList());
+    }
+
+    private static void sumOfBranches(BinaryTree root, String key) {
+        if(root.left==null && root.right==null){
+            memoSum.put("LEAFSUM_"+key+root.value, memoSum.get(key)_+root.value);
+            return;
+        }
+        String newKey = key+root.value;
+        memoSum.put(key+root.value, memoSum.get(key)+root.value);
+        sumOfBranches(root.left, newKey);
+        sumOfBranches(root.right, newKey);
     }
 
     public static class BinaryTree {
--- app/src/test/java/fuzzy/doner/BinaryTreeBranchSumTest.java
+++ app/src/test/java/fuzzy/doner/BinaryTreeBranchSumTest.java
@@ -11,6 +11,17 @@ public class BinaryTreeBranchSumTest {
     @Test
     public void branch_sum_generator(){
         BinaryTreeBranchSum.BinaryTree root;
+        root = new BinaryTreeBranchSum.BinaryTree(1);
+        root.left = new BinaryTreeBranchSum.BinaryTree(2);
+        root.right = new BinaryTreeBranchSum.BinaryTree(3);
+        root.left.left = new BinaryTreeBranchSum.BinaryTree(4);
+        root.left.right = new BinaryTreeBranchSum.BinaryTree(5);
+        root.left.left.left = new BinaryTreeBranchSum.BinaryTree(8);
+        root.left.left.right = new BinaryTreeBranchSum.BinaryTree(9);
+        root.left.right.left = new BinaryTreeBranchSum.BinaryTree(10);
+        root.right.left = new BinaryTreeBranchSum.BinaryTree(6);
+        root.right.right = new BinaryTreeBranchSum.BinaryTree(7);
+
         List<Integer> sumOfBranches = BinaryTreeBranchSum.generate();
         assertThat(sumOfBranches, is(List.of(15, 16, 18, 10, 11)));
     }

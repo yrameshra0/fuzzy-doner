ae07048299db721c30b2b474a90b69ac960fff88
Y Ramesh Rao <yrameshrao@gmail.com>
2021-12-05 16:21:07 +0530

    add: Depth first search in a graph

--- app/src/main/java/fuzzy/doner/DepthFirstSearchInGraph.java
+++ app/src/main/java/fuzzy/doner/DepthFirstSearchInGraph.java
@@ -1,9 +1,34 @@
 package fuzzy.doner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DepthFirstSearchInGraph {
-    public static List<String> traverse() {
-        return null;
+    public static List<String> traverse(Node root) {
+        List<String> accumulator = new ArrayList<>();
+        recursiveTraverse(root, accumulator);
+
+        return accumulator;
+    }
+
+    private static void recursiveTraverse(Node root, List<String> accumulator){
+        if(root!=null){
+            accumulator.add(root.name);
+        }
+
+        root.children.forEach(node->recursiveTraverse(node, accumulator));
+    }
+
+    // Do not edit the class below except
+    // for the depthFirstSearch method.
+    // Feel free to add new properties
+    // and methods to the class.
+    public static class Node {
+        String name;
+        List<Node> children = new ArrayList<>();
+
+        public Node(String name) {
+            this.name = name;
+        }
     }
 }
--- app/src/test/java/fuzzy/doner/DepthFirstSearchInGraphTest.java
+++ app/src/test/java/fuzzy/doner/DepthFirstSearchInGraphTest.java
@@ -1,13 +1,50 @@
 package fuzzy.doner;
 
+import fuzzy.doner.DepthFirstSearchInGraph.Node;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class DepthFirstSearchInGraphTest {
 
     @Test
     public void depth_first_searching_for_graph(){
-        List<String> traversal = DepthFirstSearchInGraph.traverse();
+    /*
+           A
+        /  |  \
+       B   C   D
+      / \     / \
+     E   F   G   H
+        / \   \
+       I   J   K
+    */
+        Node a = new Node("A");
+        Node b = new Node("B");
+        Node c = new Node("C");
+        Node d = new Node("D");
+        Node e = new Node("E");
+        Node f = new Node("F");
+        Node g = new Node("G");
+        Node h = new Node("H");
+        Node i = new Node("I");
+        Node j = new Node("J");
+        Node k = new Node("K");
+        a.children.add(b);
+        a.children.add(c);
+        a.children.add(d);
+        b.children.add(e);
+        b.children.add(f);
+        f.children.add(i);
+        f.children.add(j);
+        d.children.add(g);
+        d.children.add(h);
+        g.children.add(k);
+        List<String> traversal = DepthFirstSearchInGraph.traverse(a);
+        assertThat(traversal, is(List.of("A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H")));
     }
+
+
 }
